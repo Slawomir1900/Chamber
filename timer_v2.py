53bb327431afb59c2762cbd62bc6dde735bdb828
peterpirogtf <peterpirogtf@gmail.com>
2021-08-06 14:53:19 +0200

    PP commit

---
+++ timer_v2.py
@@ -0,0 +1,15 @@
+import linecache
+from time import time, ctime, sleep
+from pathlib import Path
+import pandas as pd
+
+
+class Logger():
+    def
+
+if __name__ == "__main__":
+
+    path=Path('profile.csv').absolute()
+    print(path)
+    df=pd.read_csv(filepath_or_buffer=path,sep=',')
+    print(df.head())
